69539b7ba124e35e8d3c4cc09bf7a62e13327894
iruvetar <cat6218ine@gmail.com>
2017-09-21 23:58:53 -0400

    Finish the google map update feature, which is triggered by the search button

--- service.js
+++ service.js
@@ -1,3 +1,18 @@
+var airportMap = {};
+// add a item
+airportMap["ATL"] = "ATL airport";
+airportMap["LAX"] = "LAX airport";
+airportMap["ORD"] = "ORD airport";
+airportMap["DFW"] = "DFW airport";
+airportMap["JFK"] = "JFK airport";
+airportMap["DEN"] = "DEN airport";
+airportMap["SFO"] = "SFO airport";
+airportMap["LAS"] = "LAS airport";
+airportMap["SEA"] = "SEA airport";
+airportMap["PIT"] = "PIT airport";
+airportMap["SJC"] = "SJC airport";
+
+
 $(document).ready(function(){
 //    $("#alertMessage").hide();
     $("#searchButton").on('click', scrollToMap);
@@ -8,13 +23,15 @@ function scrollToMap() {
     var place1 = $(".dropdown-toggle").first().text();
     var place2 = $(".dropdown-toggle").last().text();
     if (place1.search("Depart From") != -1 || place2.search("Depart From") != -1) {
-    $("#alertMessage").show();
-//    alert("At least one location is missing!");
+//    $("#alertMessage").show();
+    alert("At least one location is missing!");
         return;
+    } else if (place1 == place2) {
+        alert("Hey you guys actually depart from the same place?");
     } else {
         $("#alertMessage").hide();
-        console.log(place1 + " " + place2);
 //        event.preventDefault();
+        mapReset(place1, place2);
         var targetOffset = $('#googleMap').offset().top;
         $("html, body").animate({
         scrollTop: targetOffset + "px"
@@ -22,7 +39,13 @@ function scrollToMap() {
     }
     
 }
-
+function mapReset(p1, p2) {
+    console.log(p1 + " " + p2);
+    var prefix = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyD3eFiCwJPtgcaTbOE2Y4PWkL2FQqzWnQg&origin=";
+    var middle = "&destination=";
+    var end = "&mode=flying";
+    $("iframe").attr('src', prefix + airportMap[p1] + middle + airportMap[p2] + end);
+}
 function changeButtonText() {
     var destination = this.value;
     $(this).parent().prev().text(destination);
